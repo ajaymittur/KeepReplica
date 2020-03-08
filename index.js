5cc123c1692a9c3f17194c9db1fbad0b7099a18f
ajaymittur28 <ajay.cs18@bmsce.ac.in>
2020-03-08 17:36:08 +0530

    Lazy evaluation bug in PORT assignment fixed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 8000 || process.env.PORT;
+const port = process.env.PORT || 8000;
 
 // Uncomment if running locally
 // const dotenv = require("dotenv");
