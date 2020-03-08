e6807ee9fe32cbe550f8ee11fd6b511a3bb510b8
ajaymittur28 <ajay.cs18@bmsce.ac.in>
2020-03-08 17:00:45 +0530

    Fixed minor response undefined bug

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -20,7 +20,7 @@ function LoginForm(props) {
 				});
 			else setError({ message: response.data.message });
 		} catch (error) {
-			setError({ message: error.response.data.message });
+			setError({ message: error.message });
 		}
 	};
 
