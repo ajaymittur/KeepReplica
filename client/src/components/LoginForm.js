06619daa8fd323026d2a3cbf80e2657f9567a806
ajaymittur28 <ajay.cs18@bmsce.ac.in>
2020-03-08 12:45:15 +0530

    Added login page

---
+++ client/src/components/LoginForm.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import { Button, Form, Grid, Header, Message, Segment } from "semantic-ui-react";
+import axios from "axios";
+import { withRouter, Link } from "react-router-dom";
+
+function LoginForm(props) {
+	const [formData, setFormData] = useState({});
+	const [error, setError] = useState({});
+
+	const handleSubmit = async e => {
+		e.preventDefault();
+		try {
+			let response = await axios.post("/user/login", formData);
+			if (response.data.success)
+				props.history.push({
+					pathname: "/home",
+					state: {
+						displayName: response.data.displayName
+					}
+				});
+			else setError({ message: response.data.message });
+		} catch (error) {
+			setError({ message: error.response.data.message });
+		}
+	};
+
+	const handleChange = e => {
+		setFormData({
+			...formData,
+			[e.target.name]: e.target.value
+		});
+	};
+
+	return (
+		<Grid textAlign='center' style={{ height: "100vh" }} verticalAlign='middle'>
+			<Grid.Column style={{ maxWidth: 450 }}>
+				<Header as='h2' color='orange' textAlign='center'>
+					Log-in to your account
+				</Header>
+				<Form size='large'>
+					<Segment stacked>
+						<Form.Input
+							fluid
+							icon='user'
+							iconPosition='left'
+							placeholder='E-mail address'
+							name='email'
+							onChange={handleChange}
+						/>
+						<Form.Input
+							fluid
+							icon='lock'
+							iconPosition='left'
+							placeholder='Password'
+							type='password'
+							name='password'
+							onChange={handleChange}
+						/>
+
+						<Button color='orange' fluid size='large' onClick={handleSubmit}>
+							Login
+						</Button>
+					</Segment>
+				</Form>
+				<Message>
+					New to us? <Link to='/signup'>Sign Up</Link>
+				</Message>
+				{Object.entries(error).length > 0 && (
+					<Message
+						error
+						header='Could Not Sign In'
+						list={Object.keys(error).map(key => error[key])}
+						size='small'
+					/>
+				)}
+			</Grid.Column>
+		</Grid>
+	);
+}
+
+export default withRouter(LoginForm);
